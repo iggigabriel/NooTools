86d6104b9aec62071ea39ca8baddb9273b181d78
iggi <igjuricic@gmail.com>
2024-11-20 15:48:50 +0100

    Add RectPacker

---
+++ Runtime/RectPacker/PackingException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Noo.Tools.RectPacker
+{
+    public class PackingException : Exception
+    {
+        public PackingException() : base() { }
+
+        public PackingException(string message) : base(message) { }
+
+        public PackingException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
