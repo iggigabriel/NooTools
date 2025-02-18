defc715a7d6399ee3a2a3ed808b4ddf27bda6371
iggi <igjuricic@gmail.com>
2025-02-18 16:57:27 +0100

    no message

--- Runtime/SimpleTweener/NooTweener.cs
+++ Runtime/SimpleTweener/NooTweener.cs
@@ -1,10 +1,14 @@
 using Sirenix.OdinInspector;
+using System.Collections;
 using UnityEngine;
 
 namespace Noo.Tools.NooTween
 {
     public class NooTweener : MonoBehaviour
     {
+        public float delay;
+        public float speed = 1f;
+
         [SerializeField, HideLabel, InlineProperty]
         private NooTween tween;
 
@@ -12,18 +16,25 @@ namespace Noo.Tools.NooTween
 
         private void OnEnable()
         {
-            player = tween.PlayOn(gameObject);
+            StartCoroutine(Play());
         }
 
         private void OnDisable()
         {
+            StopAllCoroutines();
             player?.Dispose();
             player = null;
         }
 
+        IEnumerator Play()
+        {
+            if (delay > 0f) yield return new WaitForSeconds(delay / speed);
+            player = tween.PlayOn(gameObject);
+        }
+
         private void LateUpdate()
         {
-            player?.Update(Time.smoothDeltaTime);
+            player?.Update(Time.smoothDeltaTime * speed);
         }
     }
 }
